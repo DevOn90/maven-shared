0601d9455c4607e7dc8fd6142fd5521526fa80bb
me <me@example.com>
2026-09-21 15:29:24 +0200

    feat: add logging to FooService for entity operations

--- services/service-foo/src/main/java/com/devon90/services/service_foo/service/FooService.java
+++ services/service-foo/src/main/java/com/devon90/services/service_foo/service/FooService.java
@@ -5,9 +5,14 @@ import com.devon90.services.service_foo.repository.FooRepository;
 import com.devon90.services.service_foo.entity.Foo;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service 
 public class FooService {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(FooService.class);
+
     private final FooRepository fooRepository;
 
     public FooService(FooRepository fooRepository) {
@@ -15,10 +20,12 @@ public class FooService {
     }
 
     public List<Foo> findAll() {
+        logger.info("Fetching all Foo entities. This is test log");
         return fooRepository.findAll();
     }
 
     public Foo create(Foo foo) {
+        logger.info("Creating a new Foo entity: {}", foo);
         return fooRepository.save(foo);
     }
 }
